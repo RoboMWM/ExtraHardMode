59718c142cd43b9f8c484e0ec7997527bbd12c6a
RoboMWM <RoboMWM@users.noreply.github.com>
2016-04-13 12:08:15 -0700

    Slight optimization refactor

--- src/main/java/com/extrahardmode/features/AnimalCrowdControl.java
+++ src/main/java/com/extrahardmode/features/AnimalCrowdControl.java
@@ -51,76 +51,75 @@ public class AnimalCrowdControl extends ListenerModule {
      */
     @EventHandler
     public void onAnimalOverCrowd(CreatureSpawnEvent event) {
-        final Entity e = event.getEntity();
         World world = e.getWorld();
-
         final boolean animalOverCrowdControl = CFG.getBoolean(RootNode.ANIMAL_OVERCROWD_CONTROL, world.getName());
-        final int threshold = CFG.getInt(RootNode.ANIMAL_OVERCROWD_THRESHOLD, world.getName());
-
         //First check if config allow this feature
-        if (animalOverCrowdControl) {
+        if (!animalOverCrowdControl) 
+            return;
             
-            //Get nearby entities from newly spawn animals
-            List<Entity> cattle = e.getNearbyEntities(3, 3, 3);
-            int density = 0;
+        final Entity e = event.getEntity();    
+        final int threshold = CFG.getInt(RootNode.ANIMAL_OVERCROWD_THRESHOLD, world.getName());
+            
+        //Get nearby entities from newly spawn animals
+        List<Entity> cattle = e.getNearbyEntities(3, 3, 3);
+        int density = 0;
 
-          /**
-            *Loop and check if entity is an animal while 
-            *looping count how many animals have spawned
-            * by incrementing density
-            */
-            for (Entity a : cattle) {
-                if (a instanceof Animals
-                        && a.getType() != EntityType.HORSE
-                        && a.getType() != EntityType.WOLF
-                        && a.getType() != EntityType.OCELOT) {
-                    density++;
+      /**
+        *Loop and check if entity is an animal while 
+        *looping count how many animals have spawned
+        * by incrementing density
+        */
+        for (Entity a : cattle) {
+            if (a instanceof Animals
+                    && a.getType() != EntityType.HORSE
+                    && a.getType() != EntityType.WOLF
+                    && a.getType() != EntityType.OCELOT) {
+                density++;
+                
+                //Check if the amount of animals is bigger than the threshold given
+                if (density > threshold) {
+                    final LivingEntity animal = (LivingEntity) a;
                     
-                    //Check if the amount of animals is bigger than the threshold given
-                    if (density > threshold) {
-                        final LivingEntity animal = (LivingEntity) a;
-                        
-                      /**
-                        *This creates a runnable assign to each animals will close once
-                        *if animal is far enough from other animals or animal is dead
-                        */ 
-                        new BukkitRunnable() {
+                  /**
+                    *This creates a runnable assign to each animals will close once
+                    *if animal is far enough from other animals or animal is dead
+                    */ 
+                    new BukkitRunnable() {
 
-                            boolean firstRun = true;
+                        boolean firstRun = true;
 
-                            @Override
-                            public void run() {
-                                List<Entity> cattle = e.getNearbyEntities(3, 3, 3);
-                                int density = 0;
+                        @Override
+                        public void run() {
+                            List<Entity> cattle = e.getNearbyEntities(3, 3, 3);
+                            int density = 0;
 
-                                //this will be used to check if animal is far from other animals
-                                for (Entity a : cattle) {
-                                    if (a instanceof Animals
-                                            && a.getType() != EntityType.HORSE
-                                            && a.getType() != EntityType.WOLF
-                                            && a.getType() != EntityType.OCELOT) {
-                                        density++;
-                                    }
-                                }
-                                
-                                if (animal.isDead() || density <= threshold) {
-                                    this.cancel();
-                                } else {
-                                    /**
-                                     *Hack to force animal to move away exploits the default AI of animals
-                                     *the set Velocity make sure that no knockback is given
-                                     */
-                                    if (firstRun) {
-                                        firstRun = false;
-                                        animal.damage(0, animal);
-                                        animal.setVelocity(new Vector());
-                                    } else {
-                                        animal.damage(0.5);
-                                    }
+                            //this will be used to check if animal is far from other animals
+                            for (Entity a : cattle) {
+                                if (a instanceof Animals
+                                        && a.getType() != EntityType.HORSE
+                                        && a.getType() != EntityType.WOLF
+                                        && a.getType() != EntityType.OCELOT) {
+                                    density++;
                                 }
                             }
-                        }.runTaskTimer(this.plugin, 20, 20);
-                    }
+                            
+                            if (animal.isDead() || density <= threshold) {
+                                this.cancel();
+                            } else {
+                                /**
+                                 *Hack to force animal to move away exploits the default AI of animals
+                                 *the set Velocity make sure that no knockback is given
+                                 */
+                                if (firstRun) {
+                                    firstRun = false;
+                                    animal.damage(0, animal);
+                                    animal.setVelocity(new Vector());
+                                } else {
+                                    animal.damage(0.5);
+                                }
+                            }
+                        }
+                    }.runTaskTimer(this.plugin, 20, 20);
                 }
             }
         }
@@ -133,13 +132,15 @@ public class AnimalCrowdControl extends ListenerModule {
      */
     @EventHandler
     public void onPlayerInteract(PlayerInteractEntityEvent event) {
+        World world = player.getWorld();
+        final boolean animalOverCrowdControl = CFG.getBoolean(RootNode.ANIMAL_OVERCROWD_CONTROL, world.getName());
+        if (!animalOverCrowdControl)
+            return;
+            
         Player player = event.getPlayer();
         LivingEntity animal = (LivingEntity) event.getRightClicked();
-        World world = player.getWorld();
 
-        final boolean animalOverCrowdControl = CFG.getBoolean(RootNode.ANIMAL_OVERCROWD_CONTROL, world.getName());
-
-        if (animalOverCrowdControl && animal instanceof Animals 
+        if (animal instanceof Animals 
                 && animal.getType() != EntityType.HORSE
                 && animal.getType() != EntityType.WOLF
                 && animal.getType() != EntityType.OCELOT) {
@@ -155,12 +156,14 @@ public class AnimalCrowdControl extends ListenerModule {
      */
     @EventHandler
     public void onAnimalDeath(EntityDeathEvent event) {
-        LivingEntity animal = event.getEntity();
         World world = animal.getWorld();
-
         final boolean animalOverCrowdControl = CFG.getBoolean(RootNode.ANIMAL_OVERCROWD_CONTROL, world.getName());
-
-        if (animalOverCrowdControl && animal instanceof Animals && animal.getKiller() == null
+        if (!animalOverCrowdControl)
+            return;
+        
+        LivingEntity animal = event.getEntity();
+        
+        if (animal instanceof Animals && animal.getKiller() == null
                 && animal.getType() != EntityType.HORSE
                 && animal.getType() != EntityType.WOLF
                 && animal.getType() != EntityType.OCELOT) {
